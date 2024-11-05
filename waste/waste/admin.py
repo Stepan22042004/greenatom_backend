e39d22247d4854706eb09dec5c7185c676228bd4
stepan <mr.gerasimov.stepan@yandex.ru>
2024-11-05 19:58:21 +0000

    final

---
+++ waste/waste/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from organisations.models import (User, Capacity, Storage,
+			  Organisation, StorageCapacity,
+			  OrganisationCapacity, OrganisationStorage)
+
+admin.site.register(User)
+admin.site.register(Capacity)
+admin.site.register(Storage)
+admin.site.register(Organisation)
+admin.site.register(StorageCapacity)
+admin.site.register(OrganisationCapacity)
+admin.site.register(OrganisatinStorage)
